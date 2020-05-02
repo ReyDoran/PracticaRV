00defe38b8011061c24cab25c9b4dd1600ddb1b4
ReyDoran <manetoscopio@gmail.com>
2020-05-02 18:32:19 +0200

    Ring de carreras añadido

---
+++ Assets/Scripts/RingScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingScript : MonoBehaviour
+{
+    //Variable publica compartida que comprueba cuando se ha pasado por un aro
+    public bool ringReached;
+    //Variable que almacena si ya se ha pasado por el aro y debe desactivarse
+    private bool activated;
+    public MeshRenderer mesh;
+    public Material activatedMaterial;
+    public Material deactivatedMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Activate();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (activated)
+        {
+            Deactivate();
+            ringReached = true;
+        }
+    }
+
+    public void Activate()
+    {
+        activated = true;
+        mesh.material = activatedMaterial;
+    }
+
+    public void Deactivate()
+    {
+        activated = false;
+        mesh.material = deactivatedMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
