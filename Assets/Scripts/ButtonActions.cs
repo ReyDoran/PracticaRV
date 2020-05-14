1041e23a2c76dcf024411ce70431cf0d33ba6ef5
pauula14 <paulacalzada014@gmail.com>
2020-05-14 10:55:12 +0200

    Menu Inicial

---
+++ Assets/Scripts/ButtonActions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainGame");
+
+    }
+
+    public void ExitButton()
+    {
+        UnityEngine.Application.Quit();
+    }
+}
